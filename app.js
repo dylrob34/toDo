1e507ae47f96614620fdadb894c14b7626725818
Dylan Anderson <dylana1998@gmail.com>
2021-05-18 20:43:35 -0700

    Added index.html and basic routing

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+
+var indexRouter = require('./routes/index');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/', indexRouter);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+/* Home Page */
+router.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+})
+
+module.exports = router;
