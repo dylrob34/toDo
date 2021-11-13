51cfe7f53ec692c1f376519488f654bafc26ad62
Dylan Anderson <dylana1998@gmail.com>
2021-11-12 18:49:14 -0600

    Styled TimeBlock

--- frontend/src/component/pages/TimeBlock.jsx
+++ frontend/src/component/pages/TimeBlock.jsx
@@ -9,22 +9,35 @@ const TimeBlock = () => {
     return (
         <div>
             <div className='toolbar-container'>
-              <div className='toolbar-element' onClick={() => setPopup(true)}>
-                  <FaFolderOpen/>
-                  <div className='toolbar-item'> Categories</div>
-              </div>
-              <div className='toolbar-element'>
-                  <div className='toolbar-item'>Test Item 2</div>
-              </div>
-              <div className='toolbar-element'>
-                  <div className='toolbar-item'>Test Item 3</div>
-              </div>
-            <div className='flex-spacer-end'></div>
-            </div>
-                <div className='left-side'>
-                    <TimeTable2 />
+                <div className='toolbar-element' onClick={() => setPopup(true)}>
+                    <FaFolderOpen/>
+                    <div className='toolbar-item'> Categories</div>
                 </div>
-                <div className='right-side'>
+                <div className='toolbar-element'>
+                    <div className='toolbar-item'>Test Item 2</div>
+                </div>
+                <div className='toolbar-element'>
+                    <div className='toolbar-item'>Test Item 3</div>
+                </div>
+                <div className='flex-spacer-end'></div>
+            </div>
+            <div className='page-config'>
+                <div className='left-sidebar'></div>
+                <div className='main' name='table_metrics'>
+                    <section className='top'>
+                        <TimeTable2 />
+                    </section>
+                    <section className='bottom'>
+                        <div className='page-element' style={{backgroundColor: "red", width: "100%", height: "100%"}}>
+                            {/* Placeholder for Metrics component 1 */}
+                            placeholder
+                        </div>
+                        <div className='page-element' style={{backgroundColor: 'green', width: "100%", height: "100%" }}>
+                            {/* Placeholder for Metrics component 2 */}
+                            placeholder
+                        </div>
+                    </section>
+                </div>
             </div>
             <Popup trigger={popup} setTrigger={setPopup} >
                 <PopupCategories popup={popup} setPopup={setPopup} />
