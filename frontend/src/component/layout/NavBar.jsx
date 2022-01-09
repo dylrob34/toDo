295e9fcf6ccdc20daebe28d26069defb0706e568
RiverRipple <51027259+RiverRipple@users.noreply.github.com>
2022-01-09 17:04:20 -0500

    snazzy login page

--- frontend/src/component/layout/NavBar.jsx
+++ frontend/src/component/layout/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { logout, getLoggedIn } from "../../context/loggedInState";
 import { get } from "../../tools/request";
-import { FaUser } from 'react-icons/fa';
+import { FaUser } from "react-icons/fa";
 import { Dropdown, Option } from "./Dropdown";
 
 function NavBar({ setTeam }) {
@@ -12,17 +12,15 @@ function NavBar({ setTeam }) {
   const [teams, setTeams] = useState([]);
 
   useEffect(() => {
-    console.log(`am i logged in ${loggedIn}`)
+    console.log(`am i logged in ${loggedIn}`);
     if (loggedIn) {
-      get("/api/user/getUser")
-        .then((resJson) => {
-          console.log(resJson)
-          setName(resJson.user.firstName);
-        })
-      get("/api/teams/getTeams")
-        .then((res) => {
-          setTeams(res.teams);
-        })
+      get("/api/user/getUser").then((resJson) => {
+        console.log(resJson);
+        setName(resJson.user.firstName);
+      });
+      get("/api/teams/getTeams").then((res) => {
+        setTeams(res.teams);
+      });
     }
   }, [loggedIn]);
 
@@ -30,35 +28,63 @@ function NavBar({ setTeam }) {
     setTeam(e.target.innerHTML);
   }
 
+  console.log(window.location.href);
+
+  if (window.location.href.endsWith("/login")) {
+    return null;
+  }
+
   return (
     <div>
-      <div className='navbar-container'>
+      <div className="navbar-container">
         <div className="navbar-brand">
-          <img src="/blocks.svg" className="navbar-logo" />
+          <img alt="Blocks Logo" src="/blocks.svg" className="navbar-logo" />
           <h2>BLOCKZ</h2>
         </div>
-        {
-          !loggedIn ? (<Link to="/login" className="navbar-login">Login</Link>) : (
-            <>
-              <Dropdown first={<Link to="/todo" className='navbar-page'>ToDo</Link>}>
-                {
-                  teams.map((team) => (
-                    <Option clicked={clicked} key={team.id} value={<Link to={`/todo/${team.id}`} className='navbar-page'>{team.name}</Link>} />
-                  ))
-                }
-              </Dropdown>
-              <Link to="/capture" className='navbar-page'>Capture</Link>
-              <Link to="/timeblock" className='navbar-page'>TimeBlock</Link>
-              <div className='flex-spacer-4'></div>
-                <Link to='/account'><FaUser className="navbar-user-icon"></FaUser></Link>
-                <span className='navbar-hello'>Hello, {name}</span>
-                <button onClick={logout} className="logout-btn">Logout</button>
-            </>
-          )
-        }
+        {!loggedIn ? (
+          <Link to="/login" className="navbar-login">
+            Login
+          </Link>
+        ) : (
+          <>
+            <Dropdown
+              first={
+                <Link to="/todo" className="navbar-page">
+                  ToDo
+                </Link>
+              }
+            >
+              {teams.map((team) => (
+                <Option
+                  clicked={clicked}
+                  key={team.id}
+                  value={
+                    <Link to={`/todo/${team.id}`} className="navbar-page">
+                      {team.name}
+                    </Link>
+                  }
+                />
+              ))}
+            </Dropdown>
+            <Link to="/capture" className="navbar-page">
+              Capture
+            </Link>
+            <Link to="/timeblock" className="navbar-page">
+              TimeBlock
+            </Link>
+            <div className="flex-spacer-4"></div>
+            <Link to="/account">
+              <FaUser className="navbar-user-icon"></FaUser>
+            </Link>
+            <span className="navbar-hello">Hello, {name}</span>
+            <button onClick={logout} className="logout-btn">
+              Logout
+            </button>
+          </>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
 export default NavBar;
--- frontend/src/component/pages/Login/Login.jsx
+++ frontend/src/component/pages/Login/Login.jsx
@@ -1,85 +1,106 @@
 import React, { useState } from "react";
-import {Link, Redirect} from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { login, logout } from "../../../context/loggedInState";
 import { post } from "../../../tools/request";
 
 function LoginPage() {
-    const [error, setError] = useState("");
-    const [details, setDetails] = useState({email:'', password:''});
-    const [autoToDo, setAutoToDo] = useState(false);
+  const [error, setError] = useState("");
+  const [details, setDetails] = useState({ email: "", password: "" });
+  const [autoToDo, setAutoToDo] = useState(false);
 
+  // Function to handle on submit of the login button:
+  const submitHandler = (e) => {
+    e.preventDefault();
+    userLogin(details, setError, setAutoToDo);
+  };
 
-    // Function to handle on submit of the login button:
-    const submitHandler = e => {
-        e.preventDefault();
-        userLogin(details, setError, setAutoToDo);
-    }
-  
-    if (autoToDo === true) {
-      return (
-        <Redirect to="/todo" />
-      )
-    }
+  if (autoToDo === true) {
+    return <Redirect to="/todo" />;
+  }
 
-    //<img src="CUBE-NUMBER3-BOTTOMUP.gif" alt="Loading..." width="100%" height="50px"/>
-return (
-  <div name='Main' className='main-login'>
-    <div className='login-panel'>
-      <div className='login-element' style={{}/*{background: "no-repeat url(CUBE-NUMBER3-BOTTOMUP.gif) 50% 50%"}*/}>
-        {/* Placeholder for logo */}
-        <img src="CUBE-NUMBER3-BOTTOMUP.gif" alt="Loading..." width="100%" height="100%"/>
-
-      </div>
-      <div className='login-element'>
-        <form name='Login' onSubmit={submitHandler}>
-            <div className="form-inner">
-                {(error !=="") ? ((<div className='error'>{error}</div>)) : ""}
+  //<img src="CUBE-NUMBER3-BOTTOMUP.gif" alt="Loading..." width="100%" height="50px"/>
+  return (
+    <div name="Main" className="main-login">
+      <div className="login-panel">
+        <div className="login-content">
+          <div
+            className="login-element login-logo"
+            style={
+              {} /*{background: "no-repeat url(CUBE-NUMBER3-BOTTOMUP.gif) 50% 50%"}*/
+            }
+          >
+            <img alt="Blocks Logo" src="blocks.svg" />
+            <h2>BLOCKZ</h2>
+          </div>
+          <div className="login-element">
+            <form name="Login" onSubmit={submitHandler}>
+              <div className="form-inner">
+                {error !== "" ? <div className="error">{error}</div> : ""}
                 <div className="form-element">
-                    <label htmlFor="email" className='login-label2'>USERNAME/EMAIL: </label>
-                    <input type="email" name="email" id="email" className='login-input'
+                  <label htmlFor="email" className="login-label">
+                    USERNAME/EMAIL:{" "}
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="login-input"
                     // On change for event e, call setDetails pulling in what details already is + the value of the event (keystroke)
                     // then set the value of the state email in details to ...details + the new keystroke.
-                    onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+                    onChange={(e) =>
+                      setDetails({ ...details, email: e.target.value })
+                    }
+                    value={details.email}
+                  />
                 </div>
                 <div className="form-element">
-                    <label htmlFor="password" className='login-label2'>PASSWORD: </label>
-                    <input type="password" name="password" id='password' className='login-input'
-                    onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+                  <label htmlFor="password" className="login-label">
+                    PASSWORD:{" "}
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    className="login-input"
+                    onChange={(e) =>
+                      setDetails({ ...details, password: e.target.value })
+                    }
+                    value={details.password}
+                  />
                 </div>
-                <div className='form-item'>
-                    <input className='btn-lg' type="submit" value="LOGIN"></input>
+                <div className="form-item">
+                  <input className="btn-lg" type="submit" value="LOGIN"></input>
                 </div>
-                <Link className='btn-text' to="/signup">Create Account</Link>
-            </div>
-        </form>
+                <Link className="btn-text" to="/signup">
+                  Create Account
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-
-)
+  );
 }
 
-  const userLogin = (details, setError, setAutoToDo) => {
-    
-    //Added by Dylan to authenticate from backend
-    post("/api/auth/login", {
-      user: details.email,
-      pass: details.password
-    })
-    .then((resJson) => {
-      if (resJson.error === true) {
-        console.log("The login information didn't match...");
-        setError("The login information didn't match...");
-      } else if (resJson.loggedIn === true) {
-        console.log("logged in");
-        login(resJson.token);
-        setAutoToDo(true);
+const userLogin = (details, setError, setAutoToDo) => {
+  //Added by Dylan to authenticate from backend
+  post("/api/auth/login", {
+    user: details.email,
+    pass: details.password,
+  }).then((resJson) => {
+    if (resJson.error === true) {
+      console.log("The login information didn't match...");
+      setError("The login information didn't match...");
+    } else if (resJson.loggedIn === true) {
+      console.log("logged in");
+      login(resJson.token);
+      setAutoToDo(true);
+    }
+  });
+};
+const userLogout = () => {
+  logout();
+};
 
-      }
-    })
-  }
-  const userLogout = () => {
-    logout();
-  }
-
-  export { LoginPage }
+export { LoginPage };
