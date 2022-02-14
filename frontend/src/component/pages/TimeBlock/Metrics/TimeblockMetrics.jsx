6e3983f3862a58c5c6d7923577a33861c5e54f95
Christian Knutson <Cknutson575@gmail.com>
2022-02-14 00:56:56 -0800

    Styling and fake data done for metrics table

--- frontend/src/component/pages/TimeBlock/Metrics/TimeblockMetrics.jsx
+++ frontend/src/component/pages/TimeBlock/Metrics/TimeblockMetrics.jsx
@@ -1,6 +1,56 @@
 import React from 'react'
 
 const TimeblockMetrics = () => {
+  
+  // Test Data to mirror structure for backend - To be Used by Dylan
+  
+    let TestCategory1 = {
+      id: '1234',
+      title: 'Test Title 1',
+      color: {
+        r: '52',
+        g: '180',
+        b: '135',
+      },
+      weekHourData: {
+        Week1: 10,
+        Week2: 12,
+        Week3: 23,
+      }
+    };
+    let TestCategory2 = {
+      id: '1235',
+      title: 'Test Title 2',
+      color: {
+        r: '174',
+        g: '161',
+        b: '255',
+      },
+      weekHourData: {
+        Week1: 14,
+        Week2: 12,
+        Week3: 23,
+      }
+    };
+    let TestCategory3 = {
+      id: '1236',
+      title: 'Test Title 3',
+      color: {
+        r: '226',
+        g: '115',
+        b: '0',
+      },
+      weekHourData: {
+        Week1: 8,
+        Week2: 12,
+        Week3: 23,
+      }
+    };
+
+  let TestCategoryArray = [ TestCategory1, TestCategory2, TestCategory3, TestCategory3];
+
+  // End of Test data
+
   return (
     <div>TimeblockMetrics
 
@@ -40,7 +90,34 @@ const TimeblockMetrics = () => {
           </div>
           
         </div>
-        <div name='Table Column Titles' className='metrics-column-titles-container'>
+
+        <div className='metrics-data-table-pos'>
+          <table className='metrics-data-table' style={{overflowY:"scroll"}}>
+            <tr style={{fontSize:'.9rem'}}>
+              <td className='metrics-data-td'> Color </td>
+              <td className='metrics-data-td'> Category Name</td>
+              <td className='metrics-data-td'> Hours Spent </td>
+              <td className='metrics-data-td'> % of Total Time</td>
+            </tr>
+            {
+              TestCategoryArray.map((category) => {
+                  return (
+                      <tr style={{fontSize:'.8rem'}}>
+                        <td className='metrics-data-td'>
+                        <div style={{backgroundColor:`rgb(${category.color.r},${category.color.g},${category.color.b}`, border:'none'}} className='color-title-metrics metrics-title'></div>
+                        </td>
+                        <td className='metrics-data-td'>{category.title}</td>
+                        <td className='metrics-data-td'>{category.weekHourData.Week1}</td>
+                        <td className='metrics-data-td'>00 %</td>
+                      </tr>
+                  )
+              })
+            }
+          </table>
+        </div>
+
+
+        {/* <div name='Table Column Titles' className='metrics-column-titles-container'>
           <div style={{backgroundColor:''}} className='color-title-metrics metrics-title'>
             
           </div>
@@ -50,7 +127,7 @@ const TimeblockMetrics = () => {
         </div>
         <div name='Populated Categories'>
 
-        </div>
+        </div> */}
       </div>
 
     </div>
