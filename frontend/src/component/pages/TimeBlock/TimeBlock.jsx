117c0268a75c24266aa81cdda4970a84100756cd
Christian Knutson <Cknutson575@gmail.com>
2022-02-15 00:44:02 -0800

    Hell fucking yah...finally left side bar is looking cleannnnnn now

--- frontend/src/component/pages/TimeBlock/TimeBlock.jsx
+++ frontend/src/component/pages/TimeBlock/TimeBlock.jsx
@@ -194,34 +194,34 @@ const TimeBlock = (props) => {
   };
 
 
-  const handleMouseEnter = (e) => {
-    setDelayHandler(setTimeout(() => {
-      setToolbarCategories(true)
-    }, 1500))
-  }
-  const handleMouseLeave = () => {
-    clearTimeout(setToolbarCategories(false))
-  }
+  // const handleMouseEnter = (e) => {
+  //   setDelayHandler(setTimeout(() => {
+  //     setToolbarCategories(true)
+  //   }, 1500))
+  // }
+  // const handleMouseLeave = () => {
+  //   clearTimeout(setToolbarCategories(false))
+  // }
 
   return (
     <div>
       <div className="page-config">
         <div className="left-sidebar-sm">
-            <div className="toolbar-element" onClick={() => setModal(true)} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-              <FolderOpenIcon className="toolbar-item"/>
-              {toolbarCategories ? <div className="toolbar-item-title">Categories</div> : ''}
+            <div className="toolbar-element" onClick={() => setModal(true)} onMouseEnter={() => setToolbarCategories(true)} onMouseLeave={() => setToolbarCategories(false)}>
+              <FolderOpenIcon className="toolbar-item" style={toolbarCategories ? {transform:'translateX(.125rem)'} : {}}/>
+              <div className={"toolbar-item-title"} style={toolbarCategories ? {} : {color:"transparent", left:"7rem"}}>Categories</div>
             </div>
             <div className="toolbar-element" onMouseEnter={() => setToolbarViews(true)} onMouseLeave={() => setToolbarViews(false)}>
-              <PhotographIcon className="toolbar-item"/>
-              {toolbarViews ? <div className="toolbar-item-title">Views</div> : ""}
+              <PhotographIcon className="toolbar-item" style={toolbarViews ? {transform:'translateX(.125rem)'} : {}}/>
+              <div className="toolbar-item-title" style={toolbarViews ? {} : {color:"transparent", left:"7rem"}} >Views</div>
             </div>
             <div className="toolbar-element" onMouseEnter={() => setToolbarInsights(true)} onMouseLeave={() => setToolbarInsights(false)} >
-              <ChartPieIcon className="toolbar-item"/>
-              {toolbarInsights ? <div className="toolbar-item-title">Insights</div> : ""}
+              <ChartPieIcon className="toolbar-item" style={toolbarInsights ? {transform:'translateX(.125rem)'} : {}}/>
+              <div className="toolbar-item-title" style={toolbarInsights ? {} : {color:"transparent", left:"7rem"}}>Insights</div>
             </div>
             <div className="toolbar-element" onMouseEnter={() => setToolbarSettings(true)} onMouseLeave={() => setToolbarSettings(false)}>
-              <CogIcon className="toolbar-item"/>
-              {toolbarSettings ? <div className="toolbar-item-title">Settings</div> : ""}
+              <CogIcon className="toolbar-item" style={toolbarSettings ? {transform:'translateX(.125rem)'} : {}}/>
+              <div className="toolbar-item-title" style={toolbarSettings ? {} : {color:"transparent", left:"7rem"}}>Settings</div>
             </div>
         </div>
         <div className="main" name="table_metrics">
