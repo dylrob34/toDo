fab50b5c91cee259660d0d145b81da3cfefb3b2f
Dylan Anderson <dylana1998@gmail.com>
2022-02-14 18:57:30 -0600

    Fixed server communication with the bot

--- backend/BlockzBot.js
+++ backend/BlockzBot.js
@@ -2,27 +2,26 @@ const fetch = require('node-fetch')
 
 
 const message = (message) => {
-    try {
-        fetch(`http://localhost:3435/message`, {
-          method: "POST",
-          body: JSON.stringify(message),
-          headers: { "Content-Type": "application/json"}
-        })
-    } catch {
+    fetch(`http://localhost:3435/message`, {
+      method: "POST",
+      body: JSON.stringify(message),
+      headers: { "Content-Type": "application/json"}
+    })
+    .catch((error) => {
         console.log("Error communicating with Blockz bot")
-    }
+    })
 }
 
 const crashReport = (err) => {
-    try {
-        fetch(`http://localhost:3435/notifycrash`, {
-          method: "POST",
-          body: JSON.stringify(err),
-          headers: { "Content-Type": "application/json"}
-        })
-    } catch {
+    
+    fetch(`http://localhost:3435/notifycrash`, {
+      method: "POST",
+      body: JSON.stringify(err),
+      headers: { "Content-Type": "application/json"}
+    })
+    .catch((error) => {
         console.log("Error communicating with Blockz bot")
-    }
+    })
 }
 
 module.exports = {message, crashReport}
